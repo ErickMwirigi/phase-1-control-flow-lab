cbc7d43f8e0305675c346f059457ae14fa069ab2
ErickMwirigi <erickmbae21@gmail.com>
2023-07-05 17:40:54 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance<=400){
+    return ("This one is on me!");
+  }
+
+  else if (distance>2000 && distance < 2500){
+    return ("I will gladly take your thirty bucks.");
+  }
+
+  else if (distance>2500) {
+    return ("No can do.");
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(n){
   // Write your code here!
+  return n==="NYC"?"Ok, sounds good.":"No go.";
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(a){
   // Write your code here!
+  switch (a) {
+    case "generous":
+      return "Thank you so much."
+      break;
+
+    case "not as generous":
+        return "Thank you."
+      break;
+
+    default:
+      return "Bye."
+  }
 }
